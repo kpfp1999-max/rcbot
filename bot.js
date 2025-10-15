0cbefe9801d9330c8527863db4a882afcd7ac684
kpfp1999-max <kpfp1999@gmail.com>
2025-10-15 14:31:53 +0100

    Describe what you changed

--- bot.js
+++ bot.js
@@ -412,9 +412,7 @@ client.on("interactionCreate", async (interaction) => {
               sheet.getCell(row, 5).value = 0;
 
               // --- Always set column G to "12:00:00 AM" ---
-              const gCell = sheet.getCell(row, 6);
-              gCell.numberFormat = 'hh:mm:ss AM/PM';
-              gCell.value = "12:00:00 AM";
+              sheet.getCell(row, 6).value = "12:00:00 AM";
 
               const formulaCell = sheet.getCell(row, 7);
               if (formulaCell.formula) {
